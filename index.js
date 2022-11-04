cebb1c1497e0dcaa0cd36397b7ea4e10642077b3
Pintu-Roy121 <roypintu121@gmail.com>
2022-11-05 02:08:08 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const PORT = process.env.port || 5000;
 
 const app = express();
@@ -10,20 +11,127 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.geiv5ao.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: "UnAuthorized Access" });
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+        if (error) {
+            return res.status(401).send({ message: "UnAuthorized Access" });
+        }
+        // console.log(decoded);
+        req.decoded = decoded;
+        next();
+
+    })
+}
+
+const run = async () => {
+    try {
+        const serviceCollection = client.db('geniusCar').collection('servies');
+        const orderCollection = client.db('geniusCar').collection('orders');
 
 
+        // jwt token api ...............
+        // app.post('/jwt', (req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        //     res.send({ token })
+        // })
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+
+            res.send({ token });
+        })
+
+        // Get all services .............................
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // get specific service..............................
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+
+        })
+
+
+        //Order read(get) and write(post) Orders or save order in data base........
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            // console.log(req.query);
+
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'UnAuthorized Access' })
+            }
+
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+
+        app.post('/orders', verifyJWT, async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        // update data..........................................
+
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    status: status
+                },
+            }
+            const result = await orderCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        })
+        // Delete the data form db.................................
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+    }
+    finally {
+
+    }
+
+
+}
+run().catch(error => console.log(error))
 
 
 
